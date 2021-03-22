14f41c12413e21f4bcab56a28250ade12aef8a85
Christian <christian.garcia@ikasle.egibide.org>
2021-03-22 19:38:28 +0100

    commit factory y seeder

---
+++ resources/views/entradas/index.blade.php
@@ -0,0 +1,24 @@
+{{-- resources/views/entradas/index.blade.php --}}
+
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>Entradas</h1>
+
+    <table border="1">
+        <tr>
+            <th>Título</th>
+            <th>Texto</th>
+            <th>Fecha</th>
+        </tr>
+        @foreach($entradas as $entrada)
+            <tr>
+                <td>{{ $entrada->titulo }}</td>
+                <td>{{ $entrada->texto }}</td>
+                <td>{{ $entrada->fecha }}</td>
+            </tr>
+        @endforeach
+    </table>
+
+@endsection
