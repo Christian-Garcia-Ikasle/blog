35c5a20f3062dadda2d14dc20c9b82c59198fa5e
Christian <christian.garcia@ikasle.egibide.org>
2021-03-29 21:18:35 +0200

    Tailwind CSS aplicado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,10 +1,9 @@
-{{-- resources/views/layouts/app.blade.php --}}
-
-    <!doctype html>
+<!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <title>Mi blog</title>
 </head>
